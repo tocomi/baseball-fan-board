5b8fbbb9025e2cd772c6abaee1986f0fc376f947
tocomi <tocomi@example.com>
2018-10-20 03:07:03 +0900

    add controller

---
+++ test/controllers/api/post_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::PostControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/api/team_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::TeamControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
