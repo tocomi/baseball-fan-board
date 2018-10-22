2d80a974814d7ef1252f4a3a0f9d5ef2eb84e1b2
tocomi <tocomi@example.com>
2018-10-23 03:49:05 +0900

    change header and footer

--- app/javascript/components/common/Footer.vue
+++ app/javascript/components/common/Footer.vue
@@ -15,7 +15,8 @@ export default {
 #footer {
   position: fixed;
   bottom: 0;
-  background-color: rgb(170, 228, 255);
+  background-color: rgb(8, 53, 95);
+  color: white;
   width: 100%;
   height: 50px;
   text-align: center;
--- app/javascript/components/common/Header.vue
+++ app/javascript/components/common/Header.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  div#haeder
-    nav
+  div#header
+    nav#header-nav
       router-link(to="/") Home
 </template>
 
@@ -13,4 +13,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#header {
+  background-color: rgb(8, 53, 95);
+  width: 100%;
+  height: 50px;
+}
+#header-nav {
+  padding-top: 10px;
+  padding-left: 10px;
+}
+a {
+  color: white;
+}
 </style>
