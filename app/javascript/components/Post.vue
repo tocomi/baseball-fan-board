9ba9bec121cb2172790bd24a3b02b7420d580303
tocomi <tocomi@example.com>
2018-11-01 01:42:06 +0900

    change style

--- app/javascript/components/Post.vue
+++ app/javascript/components/Post.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div#posts
-    li(v-for="post in posts") {{ post.comment }}
+    div.comment(v-for="post in posts") {{ post.comment }}
+  
 </template>
 
 <script>
@@ -35,4 +36,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#posts {
+  float: left;
+  height: 100%;
+  width: 550px;
+  background-color: #EEEEEE;
+}
+.comment {
+  margin: 10px;
+  margin-left: 20px;
+  height: 30px;
+  width: 500px;
+  border-bottom: solid 1px #DDDDDD;
+}
 </style>
