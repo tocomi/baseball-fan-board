4313e854b12cb0e6a720ffea8ccd9cf0a915a813
tocomi <tocomi@example.com>
2018-11-13 01:22:56 +0900

    display team logo

--- app/javascript/components/TeamBanner.vue
+++ app/javascript/components/TeamBanner.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div.team-name(:class="'team-' + team.id")
     router-link.team-link(:to="{ name: 'post', params: { id: team.id } }") {{ team.name }}
+    div.logo(:class="'team-' + team.id")
 </template>
 
 <script>
@@ -23,7 +24,6 @@ export default {
   height: 50px;
   width: 500px;
   border-radius: 5px;
-  text-align: center;
   background-color: #555;
   &.team-1 {
     background-color: rgb(24, 41, 109);
@@ -37,12 +37,24 @@ export default {
       background-color: rgb(255, 217, 0);
     }
   }
-}
-.team-link {
-  display: block;
-  width: 100%;
-  height: 100%;
-  text-decoration: none;
-  color: #FFFFFF;
+  .team-link {
+    display: block;
+    padding-left: 10px;
+    width: 98%;
+    height: 100%;
+    text-decoration: none;
+    font-size: 20px;
+    color: #FFFFFF;
+  }
+  .logo {
+    margin-top: -50px;
+    margin-left: 320px;
+    width: 150px;
+    height: 50px;
+    background: url('../../assets/images/yakult.png');
+    background-size: cover;
+    opacity: 0.5;
+    background-position: 0% 40%;
+  }
 }
 </style>
